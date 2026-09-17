3b231445338896a7421c70472f939bf416a6b422
hlumelokubashe <hlumelok13@gmail.com>
2026-09-17 15:51:01 +0200

    Update POEpart1 main method comments

--- src/poepart1/POEpart1.java
+++ src/poepart1/POEpart1.java
@@ -1,3 +1,4 @@
+// Main entry point for application
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
